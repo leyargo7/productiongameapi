428283c159ef869dc78740c257225df24bb2911d
Arley Gonzalez <65101299+leyargo7@users.noreply.github.com>
2023-02-28 00:18:18 -0500

    Update index.js
    nueva url nrok

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 //const serverUrl = "https://productiongameapi-production.up.railway.app"
-const serverUrl = "https://c595-2800-e2-4f80-24a-251a-ea0d-a1b1-dab0.ngrok.io"
+//const serverUrl = "https://c595-2800-e2-4f80-24a-251a-ea0d-a1b1-dab0.ngrok.io"
+const serverUrl = "https://279e-2800-e2-4f80-24a-251a-ea0d-a1b1-dab0.ngrok.io"
 
 export function setLoading(payload) {
   return {
